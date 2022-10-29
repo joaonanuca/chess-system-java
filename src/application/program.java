f81d93f340fbc501185cefada41c74df62a86771
Joao Nanuca <joaonanuca_petro@outlook.com>
2022-10-29 20:46:14 -0300

    Projeto criado

---
+++ src/application/program.java
@@ -0,0 +1,14 @@
+package application;
+
+/**
+ *
+ * @author joaon
+ */
+public class program {
+
+    public static void main(String[] args){
+        
+        System.out.println("Hello word!");
+        
+    }
+}
